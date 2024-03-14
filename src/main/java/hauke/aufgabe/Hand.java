7b6fb57273a35ec2cd47220c561eb015732fed2a
Arkadius Hauke <arkadius@micro-devs.de>
2024-03-14 22:48:48 +0100

    Rank enum moved to Hand, comments

--- src/main/java/hauke/aufgabe/Hand.java
+++ src/main/java/hauke/aufgabe/Hand.java
@@ -9,12 +9,34 @@ import java.util.Objects;
 @NoArgsConstructor
 public class Hand {
 
+    public enum Rank {
+        HIGH_CARD,
+        PAIR,
+        TWO_PAIR,
+        THREE_OF_A_KIND,
+        STRAIGHT,
+        FLUSH,
+        FULL_HOUSE,
+        FOUR_OF_A_KIND,
+        STRAIGHT_FLUSH;
+    }
     private final List<Card> cards = new ArrayList<>();
 
+    /**
+     * Returns the cards in the hand.
+     *
+     * @return the cards in the hand (immutable copy)
+     */
     public List<Card> getCards() {
         return List.copyOf(cards);
     }
 
+    /**
+     * Adds a card to the hand.
+     *
+     * @param card the card to add
+     * @throws IllegalArgumentException if the card is null, the hand is full or the card is already in the hand
+     */
     public void addCard(Card card) throws IllegalArgumentException {
         if (Objects.isNull(card)) {
             throw new IllegalArgumentException("Card must not be null");
