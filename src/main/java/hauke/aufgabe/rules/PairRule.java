ba3b4552371b64d550c37fa0bc9035afcbc02254
Arkadius Hauke <arkadius@micro-devs.de>
2024-03-14 23:13:32 +0100

    applied improved getCardsSortedByCount in applicable

--- src/main/java/hauke/aufgabe/rules/PairRule.java
+++ src/main/java/hauke/aufgabe/rules/PairRule.java
@@ -9,12 +9,12 @@ import java.util.List;
 public class PairRule extends AbstractPokerRule<Card.Value>{
     @Override
     public boolean applicable(Hand hand) {
-        return getCardsSortedByCount(hand, 2).size() == 2;
+        return getCardsSortedByCount(hand, 2).size() == 1;
     }
 
     @Override
     public HandResult<Card.Value> rank(Hand hand) {
-        List<Card> foundPair = getCardsSortedByCount(hand, 2);
-        return new HandResult<>(Hand.Rank.PAIR, foundPair.getFirst().getValue());
+        List<Card.Value> foundPair = getCardsSortedByCount(hand, 2);
+        return new HandResult<>(Hand.Rank.PAIR, foundPair.getFirst());
     }
 }
