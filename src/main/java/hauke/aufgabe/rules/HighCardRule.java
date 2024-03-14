06fcb50796a50897d5fd4a405d219d7bd088ad42
Arkadius Hauke <arkadius@micro-devs.de>
2024-03-14 22:59:22 +0100

    unused import

--- src/main/java/hauke/aufgabe/rules/HighCardRule.java
+++ src/main/java/hauke/aufgabe/rules/HighCardRule.java
@@ -4,7 +4,6 @@ import hauke.aufgabe.Card;
 import hauke.aufgabe.Hand;
 import hauke.aufgabe.HandResult;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class HighCardRule implements PokerRule<List<Card>> {
