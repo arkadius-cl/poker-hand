ff350ff1bc211b1df90f090776c6e10403f922a6
Arkadius Hauke <arkadius@micro-devs.de>
2024-03-14 23:13:55 +0100

    initial TwoPairs rule

---
+++ src/main/java/hauke/aufgabe/rules/TwoPairsRule.java
@@ -0,0 +1,19 @@
+package hauke.aufgabe.rules;
+
+import hauke.aufgabe.Card;
+import hauke.aufgabe.Hand;
+import hauke.aufgabe.HandResult;
+
+import java.util.List;
+
+public class TwoPairsRule extends AbstractPokerRule<List<Card.Value>> {
+    @Override
+    public boolean applicable(Hand hand) {
+        return getCardsSortedByCount(hand, 2).size() == 2;
+    }
+
+    @Override
+    public HandResult<List<Card.Value>> rank(Hand hand) {
+        return null;
+    }
+}
