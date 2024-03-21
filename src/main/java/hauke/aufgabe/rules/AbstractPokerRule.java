5849be4af9278f6ba5e00c81d273737597139b88
Arkadius Hauke <arkadius.hauke@swisslife.de>
2024-03-21 14:13:15 +0100

    unused imports removed

--- src/main/java/hauke/aufgabe/rules/AbstractPokerRule.java
+++ src/main/java/hauke/aufgabe/rules/AbstractPokerRule.java
@@ -5,10 +5,7 @@ import hauke.aufgabe.Hand;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public abstract class AbstractPokerRule<T> implements PokerRule<T>{
