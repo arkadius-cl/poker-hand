643b55efc01faeea8e36aed67c2ac6b65097f208
Arkadius Hauke <arkadius.hauke@swisslife.de>
2024-03-21 15:06:22 +0100

    refactoring, should use isValidHand

--- src/main/java/hauke/aufgabe/rules/HighCardRule.java
+++ src/main/java/hauke/aufgabe/rules/HighCardRule.java
@@ -5,16 +5,25 @@ import hauke.aufgabe.Hand;
 import hauke.aufgabe.HandResult;
 
 import java.util.List;
+import java.util.stream.Stream;
 
-public class HighCardRule implements PokerRule<List<Card>> {
+public class HighCardRule extends AbstractPokerRule<List<Card>> {
     @Override
     public boolean applicable(Hand hand) {
-        return true;
+        return isValidHand(hand).isPresent();
     }
 
     @Override
     public HandResult<List<Card>> rank(Hand hand) {
         List<Card> resortedCards = hand.getCards().stream().sorted().toList().reversed();
-        return new HandResult<>(Hand.Rank.HIGH_CARD, resortedCards);
+        return isValidHand(hand)
+                .map(Hand::getCards)
+                .map(Stream::of)
+                .map(Stream::sorted)
+                .map(Stream::toList)
+                .map(List::reversed)
+                .map(cards -> {
+                    return new HandResult<>(Hand.Rank.HIGH_CARD, resortedCards);
+                }).orElse(null);
     }
 }
