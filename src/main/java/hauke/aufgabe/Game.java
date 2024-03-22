c46fb2e65b2e10dbc7b2ecf537ff818ac27d0369
Arkadius Hauke <arkadius.hauke@swisslife.de>
2024-03-22 20:12:01 +0100

    initial implementation for evaluateGame

--- src/main/java/hauke/aufgabe/Game.java
+++ src/main/java/hauke/aufgabe/Game.java
@@ -1,10 +1,13 @@
 package hauke.aufgabe;
 
+import hauke.aufgabe.problem.EvaluationException;
 import hauke.aufgabe.result.EvaluationResult;
 import hauke.aufgabe.rules.*;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @NoArgsConstructor
@@ -23,13 +26,25 @@ public class Game {
     );
 
 
-    public EvaluationResult evaluateHand(Hand hand) {
+    EvaluationResult evaluateHand(Hand hand) {
         for (EvaluationRule rule : rules) {
             if (rule.isApplicable(hand)) {
                 return rule.evaluate(hand);
             }
         }
         return null;
+    }
+
+    public String evaluateGame(List<Hand> hands) throws EvaluationException {
+        // evaluate individual hands
+        Map<String, EvaluationResult> results = hands.stream().collect(Collectors.toMap(Hand::getName, this::evaluateHand));
+        // check if there is a winner
+
+
+        // check if there are multiple winners
+
+        // evaluate hands according to rank
+        return "dupa";
     }
 
 
