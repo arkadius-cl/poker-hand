fd4399e41eeeb6bd5db8e149185501b37bea0efc
Arkadius Hauke <arkadius@connected-logistics.io>
2024-03-24 22:49:36 +0100

    more generic function moved to EvaluationResultUtils

--- src/main/java/hauke/aufgabe/util/EvaluationResultUtils.java
+++ src/main/java/hauke/aufgabe/util/EvaluationResultUtils.java
@@ -6,6 +6,8 @@ import hauke.aufgabe.rules.EvaluationResult;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public final class EvaluationResultUtils {
 
@@ -26,6 +28,59 @@ public final class EvaluationResultUtils {
                 .map(values -> values.get(index))
                 .max(Comparator.comparingInt(Card.Value::ordinal))
                 .orElse(null);
+    }
+
+    /**
+     * Removes cards from the evaluation results that match the specified filter.
+     *
+     * @param handResults   the list of evaluation results
+     * @param removalFilter the filter that determines which cards to remove
+     * @return a new list of evaluation results with the specified cards removed
+     */
+    public static List<EvaluationResult> removeFromResults(List<EvaluationResult> handResults, Function<EvaluationResult, Predicate<Card.Value>> removalFilter) {
+
+        return handResults
+                .stream()
+                .map(evalResult -> {
+                    Predicate<Card.Value> filter = removalFilter.apply(evalResult);
+                    return new EvaluationResult(evalResult.rank(), evalResult.hand(), createNewValuesFromHandCardsByFilter(filter).apply(evalResult));
+                })
+                .toList();
+    }
+
+    /**
+     * Creates a new list of card values from the hand cards of an evaluation result by applying a filter.
+     *
+     * @param removalFilter the filter that determines which cards to remove
+     * @return a function that creates a new list of card values by applying the filter
+     */
+    private static Function<EvaluationResult, List<Card.Value>> createNewValuesFromHandCardsByFilter(Predicate<Card.Value> removalFilter) {
+        return evalResult -> evalResult.hand().getCards().stream()
+                .map(Card::getValue)
+                .filter(removalFilter)
+                .sorted(Card.Value::compareTo)
+                .toList();
+    }
+
+    public static List<String> findHighestValueWinnersForIndex(List<EvaluationResult> handResults, int index) {
+        Card.Value highestCard = EvaluationResultUtils.getHighestCardForIndex(handResults, EvaluationResult::values, index);
+        return handResults.stream()
+                .filter(handResult -> handResult.values().get(index) == highestCard)
+                .map(evalResult -> evalResult.hand().getPlayerName())
+                .collect(Collectors.toList());
+    }
+
+    public static String findHighestValueWinner(List<EvaluationResult> handResults) {
+        // assume, all results have the same number of cards in value list
+        int size = handResults.getFirst().values().size();
+        List<String> winners = List.of();
+        for (int i = 0; i < size; i++) {
+            winners = EvaluationResultUtils.findHighestValueWinnersForIndex(handResults, i);
+            if (winners.size() == 1) {
+                return winners.getFirst();
+            }
+        }
+        return String.join(", ", winners);
     }
 
 
