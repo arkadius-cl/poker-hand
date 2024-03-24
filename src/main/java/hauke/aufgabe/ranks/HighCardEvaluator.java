ff04ede733d9186af1c0e9c65ac88c28ecebda2a
Arkadius Hauke <arkadius@connected-logistics.io>
2024-03-24 22:50:37 +0100

    refactored to use functions from EvaluationResultUtils

--- src/main/java/hauke/aufgabe/ranks/HighCardEvaluator.java
+++ src/main/java/hauke/aufgabe/ranks/HighCardEvaluator.java
@@ -1,36 +1,17 @@
 package hauke.aufgabe.ranks;
 
-import hauke.aufgabe.Card;
 import hauke.aufgabe.rules.EvaluationResult;
 import hauke.aufgabe.util.EvaluationResultUtils;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @NoArgsConstructor
 public class HighCardEvaluator implements RankEvaluator {
 
     @Override
     public String evaluate(List<EvaluationResult> handResults) {
-        List<String> winners = List.of();
-        for (int i = 0; i < 5; i++) {
-            winners = findWinnersForIndex(handResults, i);
-            if (winners.size() == 1) {
-                return winners.getFirst();
-            }
-        }
-        return String.join(", ", winners);
+        return EvaluationResultUtils.findHighestValueWinner(handResults);
     }
-
-    private List<String> findWinnersForIndex(List<EvaluationResult> handResults, int index) {
-        Card.Value highestCard = EvaluationResultUtils.getHighestCardForIndex(handResults, EvaluationResult::values, index);
-
-        return handResults.stream()
-                .filter(handResult -> handResult.values().get(index) == highestCard)
-                .map(evalResult -> evalResult.hand().getPlayerName())
-                .collect(Collectors.toList());
-    }
-
 
 }
